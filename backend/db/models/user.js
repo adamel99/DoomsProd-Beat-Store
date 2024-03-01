822a70e52e7541771c529a0bb889476d9a991bdb
Adam Elhamami <adamel99@gmail.com>
2024-02-29 22:38:30 -0500

    created migrations models and seeders for Users

---
+++ backend/db/models/user.js
@@ -0,0 +1,54 @@
+'use strict';
+const { Model, Validator } = require('sequelize');
+
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      User.hasMany(models.Group, { foreignKey: 'organizerId'})
+    }
+  };
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Cannot be an email.");
+            }
+          }
+        }
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+          isEmail: true
+        }
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60]
+        }
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    }, {
+    sequelize,
+    modelName: 'User'
+  }
+  );
+  return User;
+};
