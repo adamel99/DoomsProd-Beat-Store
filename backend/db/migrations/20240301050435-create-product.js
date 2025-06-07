8f2818881014d78ec09ad322c8fa6ff191713c80
Adam Elhamami <adamel99@gmail.com>
2025-06-07 07:08:51 -0400

    more ui

--- backend/db/migrations/20240301050435-create-product.js
+++ backend/db/migrations/20240301050435-create-product.js
@@ -1,6 +1,10 @@
 // migrations/<timestamp>-create-products.js
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.createTable('Products', {
